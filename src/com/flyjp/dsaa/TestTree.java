72ed77218cacb35b27d9c951a88219f8c9a68bef
flyjp9512 <flyjp@qq.com>
2019-03-14 13:53:14 +0800

    二叉树

--- src/com/flyjp/dsaa/TestTree.java
+++ src/com/flyjp/dsaa/TestTree.java
@@ -1,5 +1,22 @@
 package com.flyjp.dsaa;
 
+/**
+ * 遍历二叉树
+ * 前序遍历：根节点->左子节点->右子节点
+ * 中序遍历：左子节点->根节点->右子节点
+ * 后序遍历：左子节点->右子节点->根节点
+ *
+ * 删除二叉树结点 三种情况：
+ *  1.该节点是叶子节点，没有子节点
+ *  要删除叶子节点，只需要改变该节点的父节点的引用值，将指向该节点的引用设置为null就可以了
+ *  2.该节点有一个子节点。
+ *  改变父节点的引用，将其直接指向要删除节点的子节点
+ *  3.该节点有两个子节点
+ *  要删除有两个子节点的节点，就需要使用它的中序后继来代替该节点
+ *
+ */
+
+
 public class TestTree {
 
     public static void main(String[] args) {
@@ -8,9 +25,20 @@ public class TestTree {
         tree.insert(20,"B");
         tree.insert(15,"C");
         tree.insert(3,"D");
+        tree.insert(4,"E");
+        tree.insert(90,"F");
         System.out.println(tree.root.leftChild.data);//3
 
         System.out.println(tree.find(3).str);//D
+        //前序遍历
+        tree.frontOrder(tree.root);//10 3 4 20 15 90
+        System.out.println("-----------");
+        //中序遍历
+        tree.inOrder(tree.root);//3 4 10 15 20 90
+        System.out.println("-----------");
+        //后序遍历
+        tree.afterOrder(tree.root);//4 3 15 90 20 10
+
     }
 }
 
@@ -86,11 +114,132 @@ class Tree{
 
     }
 
+
     /**
      * 删除节点
      */
-    public void delete(long value){
+    public boolean delete(long value){
+        //引用当前节点，从根节点开始
+        TreeNode current = root;
+        //应用当前节点的父节点
+        TreeNode parent = root;
+        //是否为左节点
+        boolean isLeftChild = true;
+        while(current.data != value){
+            if(current.data > value){
+                current = current.leftChild;
+                isLeftChild = true;
+            }else{
+                current = current.rightChild;
+                isLeftChild = false;
+            }
+            if(current == null){
+                return  false;
+            }else{
+                //删除叶子节点，也就是该节点没有子节点
+                if(current.leftChild == null && current.rightChild == null){
+                    //如果删除节点是根节点 为null
+                    if(current == root){
+                        root = null;
+                    } else if(isLeftChild){//如果是父节点的左子节点
+                        parent.leftChild = null;
+                    }else{
+                        parent.rightChild = null;
+                    }
+                    return true;
+                    //只有一个子节点，改变父节点的引用，将其直接指向要删除节点的子节点
+                }else if(current.leftChild == null || current.rightChild == null){
+                    if(current == root){
+                        root = current.leftChild!=null?
+                                current.leftChild:current.rightChild;
+                    } else if(isLeftChild){
+                        parent.leftChild = current.leftChild!=null?
+                                current.leftChild:current.rightChild;
+                    }else{
+                        parent.rightChild = current.leftChild!=null?
+                                current.leftChild:current.rightChild;
+                    }
+                }else{
+                    TreeNode successor = getSuccessor(current);
+                    if(current == root){
+                        root = successor;
+                    }else if(isLeftChild){
+                        parent.leftChild = successor;
+                    }else{
+                        parent.rightChild = successor;
+                    }
+                    successor.leftChild = current.leftChild;
+                }
 
+            }
+
+        }
+        return false;
+    }
+
+
+    /**
+     * 中序后继节点
+     */
+    public TreeNode getSuccessor(TreeNode delNode){
+        TreeNode successor = delNode;
+        TreeNode successorParent = delNode;
+        TreeNode current = delNode.rightChild;
+        while(current != null){
+            successorParent = successor;
+            successor = current;
+            current = current.leftChild;
+        }
+        if(successor != delNode.rightChild){
+            //把中序后继节点等于
+            successorParent.leftChild = successor.rightChild;
+            successor.rightChild = delNode.rightChild;
+        }
+        return successor;
+    }
+
+    /**
+     * 前序遍历
+     */
+    public void frontOrder(TreeNode localNode){
+        if(localNode != null){
+            //访问根节点
+            System.out.println(localNode.data + "," +localNode.str);
+            //前序遍历左子树
+            frontOrder(localNode.leftChild);
+            //前序遍历右子树
+            frontOrder(localNode.rightChild);
+        }
+    }
+
+    /**
+     * 中序遍历
+     */
+    public void inOrder(TreeNode localNode){
+        if(localNode != null){
+
+            //中序遍历左子树
+            inOrder(localNode.leftChild);
+            //访问根节点
+            System.out.println(localNode.data + "," +localNode.str);
+            //中序遍历右子树
+            inOrder(localNode.rightChild);
+        }
+    }
+
+    /**
+     * 后序遍历
+     */
+    public void afterOrder(TreeNode localNode){
+        if(localNode != null){
+
+            //后序遍历左子树
+            afterOrder(localNode.leftChild);
+            //后序遍历右子树
+            afterOrder(localNode.rightChild);
+            //访问根节点
+            System.out.println(localNode.data + "," +localNode.str);
+        }
     }
 
 }
