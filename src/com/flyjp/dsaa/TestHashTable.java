2f906cd569c3bcba3d1716dba32334a6b5d043a9
flyjp9512 <flyjp@qq.com>
2019-03-14 21:32:35 +0800

    哈希表实现：开放地址法

--- src/com/flyjp/dsaa/TestHashTable.java
+++ src/com/flyjp/dsaa/TestHashTable.java
@@ -4,24 +4,27 @@ import java.math.BigInteger;
 
 /**
  * 哈希表
+ * 开放地址法 在表中开放一个null的地址 缺陷;会占用别人的位置
+ * 链地址法
  */
 public class TestHashTable {
 
     public static void main(String[] args) {
         HashTable hashTable = new HashTable();
-        hashTable.insert(new Info("abc","张三"));
-        hashTable.insert(new Info("cba","李四"));
-        hashTable.insert(new Info("bbb","王五"));
+        hashTable.insert(new Info("a","张三"));
+        hashTable.insert(new Info("ct","李四"));
+        hashTable.insert(new Info("b","王五"));
 
-        System.out.println(hashTable.find("abc").getName());
-        System.out.println(hashTable.find("cba").getName());//李四
-        System.out.println(hashTable.find("bbb").getName());//李四
+        System.out.println(hashTable.find("a").getName());
+        System.out.println(hashTable.find("ct").getName());//李四
+        System.out.println(hashTable.find("b").getName());//李四
     }
 }
 
 class HashTable{
 
     private Info[] arr;
+
     /**
      * 默认构造
      */
@@ -37,17 +40,55 @@ class HashTable{
     }
 
     /**
-     * 插入数据
+     * 插入数据  开放地址法
      */
     public void insert(Info info){
-        arr[hashCode(info.getKey())] = info;
+        //获得关键字
+        String key = info.getKey();
+        //关键字所定义的哈希数
+        int hashVal = hashCode(key);
+        //如果这个索引已经被占用，而且里面是一个未被删除的数据
+        while(arr[hashVal] != null && arr[hashVal].getName() != null){
+            //进行递增
+            ++hashVal;
+            hashVal %= arr.length;
+        }
+        arr[hashVal] = info;
+    }
+
+
+
+    /**
+     * 查找数据 开放地址法
+     */
+    public Info find(String key){
+        int hashVal = hashCode(key);
+        //不为空
+        while(arr[hashVal] != null){
+            if(arr[hashVal].getKey().equals(key)){
+                return arr[hashVal];
+            }
+            ++hashVal;
+            hashVal &= arr.length;
+        }
+        return null;
     }
 
     /**
-     * 查找数据
+     * 删除数据 开放地址法
      */
-    public Info find(String key){
-        return arr[hashCode(key)];
+    public Info delete(String key){
+        int hashVal = hashCode(key);
+        while(arr[hashVal] != null){
+            if(arr[hashVal].getKey().equals(key)){
+                Info tmp = arr[hashVal];
+                tmp.setName(null);
+                return tmp;
+            }
+            ++hashVal;
+            hashVal %= arr.length;
+        }
+        return null;
     }
 
     /**
@@ -74,7 +115,7 @@ class HashTable{
             hashVal = hashVal.add(letter.multiply(pow27)) ;
             pow27 = pow27.multiply(new BigInteger(String.valueOf(27)));
         }
-                //压缩后仍然能出现问题
+        //压缩后仍然能出现问题
         return  hashVal.mod(new BigInteger(String.valueOf(arr.length))).intValue() ;
 
     }
@@ -111,4 +152,8 @@ class Info{
     public void setName(String name) {
         this.name = name;
     }
+
 }
+
+
+
